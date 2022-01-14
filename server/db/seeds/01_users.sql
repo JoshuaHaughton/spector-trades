a8983885c35afe2793c6910c98d4a138d06d11f0
Drumshtick <ekodnar@outlook.com>
2022-01-14 15:00:42 -0600

    updated seeds to use seed files for avatars

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,5 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (username, email, password_digest, avatar_url, created_at) VALUES ('alice', 'test@example.com', '12341234', 'avatar1.jpg', NOW());
-INSERT INTO users (username, email, password_digest, avatar_url, created_at) VALUES ('bob', 'test2@example.com', '123123123', 'avatar2.jpg', NOW());
-INSERT INTO users (username, email, password_digest, avatar_url, created_at) VALUES ('fred', 'test2@example.com', '123123123', 'avatar2.jpg', NOW());
+INSERT INTO users (username, email, password_digest, avatar_url, created_at) VALUES ('alice', 'test@example.com', '12341234', 'woman1.png', NOW());
+INSERT INTO users (username, email, password_digest, avatar_url, created_at) VALUES ('bob', 'test2@example.com', '123123123', 'nerd1.png', NOW());
+INSERT INTO users (username, email, password_digest, avatar_url, created_at) VALUES ('fred', 'test2@example.com', '123123123', 'male1.png', NOW());
 
