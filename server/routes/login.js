db4ec3c13a5fc8616583b74e5b220b31dd2e89fd
Drumshtick <ekodnar@outlook.com>
2022-01-16 14:27:33 -0600

    cleaned up comments moved helper to helpers

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,45 +1,44 @@
 const express = require('express');
 const app = express.Router();
 const bcrypt = require('bcrypt');
-
+const { getUserByEmail } = require('./helpers/user-helpers');
 // curl -X POST -d 'password=password' -d 'email=test@example.com' http://localhost:3002/api/login
 
 //Default route is /api/login
 module.exports = (db) => {
-  app.post('/', (req, res) => {
-
+  app.post("/", (req, res) => {
     console.log(req.body);
 
+    // if (req.session['user_id']) {
+    //   console.log("user cookie already exists, user id: ", req.session.user_id);
+    // }
+
     const { password, email } = req.body;
-    checkEmail(email, db)
-    .then(async resp => {
-      await bcrypt.compare(password, resp.password_digest, function (err, result) {
-        console.log(result)
-        if (!result) {
-          console.log("success!")
-          return res.send({status: 200, user_id: resp.user_id})
-        }
-        console.log("password is incorrect")
+    getUserByEmail(email, db)
+      .then(async (resp) => {
+        console.log(password,  resp.password_digest);
+        await bcrypt.compare(
+          password,
+          resp.password_digest,
+          function (err, result) {
+            console.log(result);
+            if (result) {
+              console.log("success!");
+
+              // req.session['user_id'] = resp.user_id;  // SEND COOKIE TO CLIENT
+
+              return res.send({ status: 200, user_id: resp.user_id });
+            } else {
+              console.log("password is incorrect");
+              return res.status(401).send("password is incorrect");
+            }
+          }
+        );
       })
-    }).catch(err => {
-      console.log(err);
-    })
-
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
-
-return app;
-}
-
-const checkEmail = function(email, db) {
-  return db.query(
-    `
-    SELECT * FROM users
-    WHERE email = $1;
-    `, [ email ])
-    .then(response => {
-    return response.rows[0];
-  }).catch(err => {
-    console.log("ERROR in checkEmail", err.message);
-  });
-}
+  return app;
+};
