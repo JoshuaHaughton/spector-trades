2b2c09d92f0ec7ef862b4d1f2f5383be547a4709
Drumshtick <ekodnar@outlook.com>
2022-01-16 17:53:06 -0600

    changed lookup function

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,37 +1,34 @@
 const express = require('express');
 const app = express.Router();
 const bcrypt = require('bcrypt');
-const { getUserByEmail } = require('./helpers/user-helpers');
+const { getUserByColumn } = require('./helpers/user-helpers');
 // curl -X POST -d 'password=password' -d 'email=test@example.com' http://localhost:3002/api/login
 
 //Default route is /api/login
 module.exports = (db) => {
   app.post("/", (req, res) => {
-    console.log(req.body);
 
     // if (req.session['user_id']) {
     //   console.log("user cookie already exists, user id: ", req.session.user_id);
     // }
 
     const { password, email } = req.body;
-    getUserByEmail(email, db)
+    getUserByColumn( 'email', email, db)
       .then(async (resp) => {
-        console.log(password,  resp.password_digest);
         await bcrypt.compare(
           password,
           resp.password_digest,
           function (err, result) {
-            console.log(result);
             if (result) {
-              console.log("success!");
+              console.log("successful login from: ", resp.username);
 
               // req.session['user_id'] = resp.user_id;  // SEND COOKIE TO CLIENT
 
               return res.send({ status: 200, user_id: resp.user_id });
-            } else {
-              console.log("password is incorrect");
-              return res.status(401).send("password is incorrect");
             }
+              console.log("password is incorrect");
+              console.log(err)
+              return res.status(401).send("password is incorrect");
           }
         );
       })
--- server/routes/register.js
+++ server/routes/register.js
@@ -2,12 +2,16 @@
 // ON SUCCESS SENDS
 // {status: 200, user_id: x}
 // user_id is the id of the new entry
-// ON FAILURE sends 422 (Unprocessable Entity) see below
-// https://stackoverflow.com/questions/3050518/what-http-status-response-code-should-i-use-if-the-request-is-missing-a-required
+// ON FAILURE sends 422 (Unprocessable Entity)
+
 const express = require('express');
 const app = express.Router();
+
 const bcrypt = require('bcrypt');
 const saltRounds = 8; // SaltRounds should be over 10 in production
+
+const { verifyUniqueColumn } = require('./helpers/user-helpers');
+
 // File upload dependencies
 const multer = require("multer");
 const storage = multer.diskStorage({
@@ -17,6 +21,7 @@ const storage = multer.diskStorage({
   }
 });
 const upload = multer({ storage: storage});
+
 //Default route is /api/register
 // form fields are username, email, password, avatar
 // curl -X POST -F 'username=linuxize' -F 'email=linuxize@example.com' -F 'password=password' -F 'avatar=@image.png' http://localhost:3001/api/register
@@ -29,31 +34,28 @@ module.exports = (db) => {
       return res.sendStatus(422);
     }
     const { username, email, password } = req.body;
-    let avatar_url;
-    if (req.file) {
-      avatar_url = req.file.filename;
-    }
-
+    const avatar_url = req.file ? req.file.filename : null;
+    
+    console.log(verifyUniqueColumn())
 
     bcrypt.hash(password, saltRounds, function (err, password_digest) {
       if (err) {
         console.log("bcrypt hash err: ", err)
+        return res.send({status: 500, error: "hashing"})
       }
+
       const user = {
         username,
         email,
-        password_digest
-      } 
-      if (avatar_url) {
-        user["avatar_url"] = avatar_url;
-      } else {
-        user["avatar_url"] = null;
-      }
+        password_digest,
+        avatar_url
+      };
+
       addUser(user, db).then(resp => {
         const responseString = { status: 200, user_id: resp.id };
         if (avatar_url) console.log('user avatar inserted in db');
         console.log("New user added");
-        console.log(responseString)
+        console.log("response sent to client: ", responseString)
         return res.send(responseString);
       }).catch(err => {
         // NOTE
