52ae81c69dd5bc38f51f33d1ef9e54052f504f74
Drumshtick <ekodnar@outlook.com>
2022-01-16 21:13:36 -0600

    implemented verifyUniqueColumns

--- server/routes/register.js
+++ server/routes/register.js
@@ -10,7 +10,7 @@ const app = express.Router();
 const bcrypt = require('bcrypt');
 const saltRounds = 8; // SaltRounds should be over 10 in production
 
-const { verifyUniqueColumns } = require('./helpers/user-helpers');
+const { verifyUniqueColumns, addUser } = require('./helpers/user-helpers');
 
 // File upload dependencies
 const multer = require("multer");
@@ -37,63 +37,45 @@ module.exports = (db) => {
     const avatar_url = req.file ? req.file.filename : null;
     
     verifyUniqueColumns({email, username}, db)
-    .then(resp => {
-    console.log("verifyUniqueColumn: ", resp);
-
+    .then(result => {
+      if (!result) {
+        // 409 Conflict
+        console.log("DUPLICATE EMAIL OR USERNAME")
+        return res.send({status: 409, message: "username or email already in use"})
+      }
+      bcrypt
+        .hash(password, saltRounds, function (err, password_digest) {
+          if (err) {
+            console.log("bcrypt hash err: ", err)
+            return res.send({status: 500, message: "hashing error - bad password"})
+          }
+    
+          const user = {
+            username,
+            email,
+            password_digest,
+            avatar_url
+          };
+    
+          addUser(user, db).then(resp => {
+            const responseString = { status: 200, user_id: resp.id };
+            if (avatar_url) console.log('user avatar inserted in db');
+            console.log("New user added");
+            console.log("response sent to client: ", responseString)
+            return res.send(responseString);
+          }).catch(err => {
+            console.log('ERROR in db insert for user registration', err.message);
+          });
+        })
     })
     .catch(resp => {
-    console.log("verifyUniqueColumn: ", resp);
+    console.log("ERROR in verifyUniqueColumn: ", resp);
     });
 
-    bcrypt.hash(password, saltRounds, function (err, password_digest) {
-      if (err) {
-        console.log("bcrypt hash err: ", err)
-        return res.send({status: 500, error: "hashing"})
-      }
-
-      const user = {
-        username,
-        email,
-        password_digest,
-        avatar_url
-      };
-
-      addUser(user, db).then(resp => {
-        const responseString = { status: 200, user_id: resp.id };
-        if (avatar_url) console.log('user avatar inserted in db');
-        console.log("New user added");
-        console.log("response sent to client: ", responseString)
-        return res.send(responseString);
-      }).catch(err => {
-        // NOTE
-        // Are we going to verify serverside or clientside if an email has been duplicated
-        // in Users
-        console.log('ERROR in db insert for user registration', err.message);
-      });
-    })
+    
 
   });
 
 
 return app;
-}
-
-const addUser = function(user, db) {
-  let queryString = 
-                    `
-                    INSERT INTO 
-                    users (username, email, password_digest, avatar_url)
-                    VALUES ($1, $2, $3, $4)
-                    RETURNING *;
-                    `;
-
-  const queryParams = [ user.username, user.email, user.password_digest, user.avatar_url ];
-
-  return db.query(queryString, queryParams)
-  .then((result) => {
-    return result.rows[0];
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
 };
