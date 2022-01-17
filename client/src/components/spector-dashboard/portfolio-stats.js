06aa597cc515fb40c6d63dc273671d41e6994445
Ken Cruz <ken@kencruz.ca>
2022-01-17 18:27:32 -0500

    added portfolio stat component placeholders

--- client/src/components/spector-dashboard/portfolio-stats.js
+++ client/src/components/spector-dashboard/portfolio-stats.js
@@ -1,12 +1,14 @@
-import { Card, CardContent} from '@mui/material';
-
+import {  Stack } from '@mui/material';
+import { ProfitStat } from './profit-stat';
+import { GrowthStat } from './growth-stat';
+import { SpecBalanceStat } from './spec-balance-stat';
 
 export const PortfolioStats = () => (
-    <Card
-      sx={{ height: '100%' }}
-    >
-      <CardContent>
-        PortFolio Stats
-      </CardContent>
-    </Card>
+   <Stack spacing={3} 
+          sx={{height: '100%'}} 
+          justifyContent='space-between'>
+       <ProfitStat />
+       <GrowthStat />
+       <SpecBalanceStat />
+   </Stack>
   );
