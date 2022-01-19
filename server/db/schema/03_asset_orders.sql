c00d930ea84d4e0c1758a77f3b0d1ea089630297
Drumshtick <ekodnar@outlook.com>
2022-01-19 14:04:07 -0600

    added column for exit point CAN BE NULL

--- server/db/schema/03_asset_orders.sql
+++ server/db/schema/03_asset_orders.sql
@@ -8,5 +8,6 @@ CREATE TABLE asset_orders(
   created_at DATE NOT NULL DEFAULT NOW(),
   user_id INTEGER REFERENCES users(id),
   portfolio_id INTEGER REFERENCES portfolios(id),
-  sold BOOLEAN NOT NULL
+  sold BOOLEAN NOT NULL,
+  exit_point INTEGER
 );
