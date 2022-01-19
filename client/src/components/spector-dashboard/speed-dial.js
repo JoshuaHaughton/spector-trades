216169931e4640001b5a17b111adaa1bdfd2dc6e
Ken Cruz <ken@kencruz.ca>
2022-01-19 08:57:29 -0500

    Implemented speed dial

---
+++ client/src/components/spector-dashboard/speed-dial.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Backdrop } from '@mui/material';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
+
+const actions = [
+  { icon: <SaveIcon />, name: 'Add Investment' },
+  { icon: <FileCopyIcon />, name: 'Add Portfolio' },
+  // { icon: <PrintIcon />, name: 'Print' },
+  // { icon: <ShareIcon />, name: 'Share' },
+];
+
+export const SpectorSpeedDial = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Backdrop open={open} sx={{ zIndex: (theme) => theme.zIndex.speedDial - 1 }} />
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+          />
+        ))}
+      </SpeedDial>
+    </>
+  );
+}
