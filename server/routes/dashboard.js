b849d364bf7d76f50a7f9b6fcc339393950e6c5b
Drumshtick <ekodnar@outlook.com>
2022-01-18 21:09:33 -0600

    started dashboard get route

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express.Router();
-
+/* NOTE THAT THE TOKEN STRING MUST BE GENERATED BASED ON AN EXISTING USER IN YOUR DB
+curl -X GET http://localhost:3001/api/dashboard -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo0LCJ1c2VyX2VtYWlsIjoiZWF0ZGVtQGNvb2tpZXMuY29tIiwiaWF0IjoxNjQyNTI5NTAyfQ.I4wkKqnv9fuPCxHob8dIwOrrlzF-F_FLvT2r5bTtROs" -H "Content-Type: application/json"
+*/
 //Default route is /api/dashboard
 module.exports = () => {
   app.get('/', (req, res) => {
@@ -11,3 +13,19 @@ module.exports = () => {
   });
   return app;
 }
+const getPortfolioData = (options, db) => {
+  const queryParams = [options.user_id];
+  let queryString = `
+      SELECT * FROM portfolios
+      WHERE user_id = $1
+    `;
+  return db.query(queryString, queryParams)
+    .then(response => {
+      return response.rows;
+    })
+    .catch(err => {
+      console.log(`ERROR in getPortfoliosByUser user_id: ${options.user_id}`);
+      console.log(err.message)
+      return err;
+    });
+};
