9d036a03795865a4a50f1f1c262eea9da370602f
Drumshtick <ekodnar@outlook.com>
2022-01-20 16:10:08 -0600

    bug fix for asset_order total (didn't multiply for amount of units)

--- server/db/seeds/03_asset_orders.sql
+++ server/db/seeds/03_asset_orders.sql
@@ -1,14 +1,14 @@
 INSERT INTO asset_orders (name, type, units, price_at_purchase, user_id, portfolio_id, sold) 
-VALUES ('John', 'Cryptocurrency', 4.32, 1000, 1, 1, false);
+VALUES ('John', 'Cryptocurrency', 1.32, 1000, 1, 1, false);
 
 INSERT INTO asset_orders (name, type, units, price_at_purchase, user_id, portfolio_id, sold) 
 VALUES ('Leon', 'Stocks', 1.01, 11302, 3, 3, false);
 
 INSERT INTO asset_orders (name, type, units, price_at_purchase, user_id, portfolio_id, sold) 
-VALUES ('Bill', 'Cryptocurrency', 23.44, 23453, 2, 2, false);
+VALUES ('Bill', 'Cryptocurrency', 2, 23453, 2, 2, false);
 
 INSERT INTO asset_orders (name, type, units, price_at_purchase, user_id, portfolio_id, sold) 
-VALUES ('ETH', 'Cryptocurrency', 23.44, 23453, 2, 2, false);
+VALUES ('ETH', 'Cryptocurrency', 2, 23453, 2, 2, false);
 
 INSERT INTO asset_orders (name, type, units, price_at_purchase, user_id, portfolio_id, sold) 
-VALUES ('ETH', 'Cryptocurrency', 23.44, 23453, 2, 2, true);
+VALUES ('ETH', 'Cryptocurrency', 2, 23453, 2, 2, true);
