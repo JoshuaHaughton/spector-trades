68eb858cd6923b48bc2ada692790eb294c62c840
Drumshtick <ekodnar@outlook.com>
2022-01-20 15:43:08 -0600

    imported cutom styles

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -6,7 +6,7 @@ import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { createEmotionCache } from '../utils/create-emotion-cache';
 import { theme } from '../theme';
-
+import '../../public/static/styles/dashboard-button.css'
 const clientSideEmotionCache = createEmotionCache();
 
 const App = (props) => {
