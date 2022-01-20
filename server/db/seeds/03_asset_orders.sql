436265fde4659bcad439e05e7d9f4371a289dc0f
Drumshtick <ekodnar@outlook.com>
2022-01-20 17:03:30 -0600

    fixed seeds

--- server/db/seeds/03_asset_orders.sql
+++ server/db/seeds/03_asset_orders.sql
@@ -1,14 +1,14 @@
-INSERT INTO asset_orders (name, type, units, price_at_purchase, user_id, portfolio_id, sold) 
-VALUES ('John', 'Cryptocurrency', 1.32, 1000, 1, 1, false);
+INSERT INTO asset_orders (name, type, units, price_at_purchase, created_at, user_id, portfolio_id, sold) 
+VALUES ('John', 'Cryptocurrency', 1.32, 1000, '2021-01-20T06:00:00.000Z', 1, 1, false);
 
-INSERT INTO asset_orders (name, type, units, price_at_purchase, user_id, portfolio_id, sold) 
-VALUES ('Leon', 'Stocks', 1.01, 11302, 3, 3, false);
+INSERT INTO asset_orders (name, type, units, price_at_purchase, created_at, user_id, portfolio_id, sold) 
+VALUES ('Leon', 'Stocks', 1.01, 11302, '2021-01-20T06:00:00.000Z', 3, 3, false);
 
-INSERT INTO asset_orders (name, type, units, price_at_purchase, user_id, portfolio_id, sold) 
-VALUES ('Bill', 'Cryptocurrency', 2, 23453, 2, 2, false);
+INSERT INTO asset_orders (name, type, units, price_at_purchase, created_at, user_id, portfolio_id, sold) 
+VALUES ('Bill', 'Cryptocurrency', 2, 23453, '2021-02-20T06:00:00.000Z', 2, 2, false);
 
-INSERT INTO asset_orders (name, type, units, price_at_purchase, user_id, portfolio_id, sold) 
-VALUES ('ETH', 'Cryptocurrency', 2, 23453, 2, 2, false);
+INSERT INTO asset_orders (name, type, units, price_at_purchase, created_at, user_id, portfolio_id, sold) 
+VALUES ('ETH', 'Cryptocurrency', 2, 23453, '2021-03-20T06:00:00.000Z', 2, 2, false);
 
-INSERT INTO asset_orders (name, type, units, price_at_purchase, user_id, portfolio_id, sold) 
-VALUES ('ETH', 'Cryptocurrency', 2, 23453, 2, 2, true);
+INSERT INTO asset_orders (name, type, units, price_at_purchase, created_at, user_id, portfolio_id, sold) 
+VALUES ('ETH', 'Cryptocurrency', 2, 23453, '2021-04-20T06:00:00.000Z', 2, 2, true);
