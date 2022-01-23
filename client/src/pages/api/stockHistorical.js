07ac56046777f0c97298d7184a7d3a9a673436c9
Drumshtick <ekodnar@outlook.com>
2022-01-23 13:01:23 -0600

    fixed bug

--- client/src/pages/api/stockHistorical.js
+++ client/src/pages/api/stockHistorical.js
@@ -1,6 +1,29 @@
 import axios from "axios";
 const fs = require("fs");
+const moment = require('moment');
+function readFile (srcPath) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(srcPath, 'utf8', function (err, data) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
 
+function writeFile (savPath, data, ) {
+  return new Promise(function (resolve, reject) {
+    fs.writeFile(savPath, data, function (err) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+}
 
 export default async (req, res) => {
   if (!req.body['id']) {
@@ -8,121 +31,92 @@ export default async (req, res) => {
   }
   const id = req.body['id'];
 
-  fs.readFile('src/pages/api/stockHistorical.json', 'utf8', function readFileCallback(err, data){
-    if (err){
-      console.log("READFILE ERR in stockHistorical: ", err)
-      return res.status(200).send(err);
-      // return res.status(500).send(err);
-    } else {
-    let stockHistory = JSON.parse(data); //now it an object
+  let needsSearch;
+  let stockHistory;
+  let storedData;
+  readFile('src/pages/api/stockHistorical.json').then((data) => {
+    stockHistory = JSON.parse(data); //now it an object
+    storedData = findStocks(id, stockHistory)[0];
+    needsSearch = findStocks(id, stockHistory)[1];
+    console.log("keys left to search for: ", needsSearch);
 
-    const [storedData, remainingSearchKeys] = findStocks(id, stockHistory);
-    console.log("keys left to search for: ", remainingSearchKeys)
     const options = {
       method: 'GET',
       url: 'https://twelve-data1.p.rapidapi.com/time_series',
-      params: {symbol: remainingSearchKeys.join(','), interval: '1day', outputsize: '365', format: 'json'},
+      params: {symbol: id.join(','), interval: '1day', outputsize: '365', format: 'json'},
       headers: {
         'x-rapidapi-host': 'twelve-data1.p.rapidapi.com',
         'x-rapidapi-key': '4da8bbba96mshde71119bcd05e66p152d89jsn9db02680c1dd'
       }
     };
-    if (remainingSearchKeys.length !== 0) {
-    console.log("MAKING REQUEST TO twelve-data-api for: ", remainingSearchKeys)
-      axios.request(options).then(function (response) {
-        // console.log(response.data)
-        const respKeys = Object.keys(response.data);
-        respKeys.forEach(key => {
-          stockHistory[key] = response.data[key];
-        });
-        const json = JSON.stringify(stockHistory);
-        fs.writeFile('src/pages/api/stockHistorical.json', json, 'utf8', function readFileCallback(err, data) {
-          if (err) {
-            console.log("error writing back to json, cryptoHistorical", err)
-            // console.log(json);
-          }
-          console.log("data: ",data)
-          remainingSearchKeys.forEach(key => {
-            stockHistory[key] = response.data[key];
-          });
-          return res.status(200).json(stockHistory);
-        });
 
-      }).catch(function (error) {
-        console.error(error);
-      });
-    } else {
-      console.log("NO SEARCH REQUIRED IN STOCKSHISTORICAL")
-      // console.log("storedData sent: ", storedData)
-      return res.status(200).json(storedData);
+    if (needsSearch.includes(true)) {
+      console.log("MAKING REQUEST TO twelve-data-api for: ", needsSearch);
+      return axios.request(options);
     }
 
-  }
-  });
+    // return storedData if no remaining keys
+    return new Promise((resolve, reject) => {
+      resolve({storedData});
+    });
+
+  }).then(response => {
+    if (response['storedData']) {
+      console.log("NO SEARCH REQUIRED IN STOCKSHISTORICAL for: ", id)
+      return res.status(200).json(response.storedData);
+    }
+
+    if (response) {
+      // console.log(response.data)
+      const respKeys = Object.keys(response.data);
+      respKeys.forEach(key => {
+        stockHistory[key] = response.data[key];
+      });
+      const json = JSON.stringify(stockHistory);
+
+      id.forEach(key => {
+        stockHistory[key] = response.data[key];
+      });
+
+      res.status(200).json(stockHistory);
+
+      return writeFile('src/pages/api/stockHistorical.json', json);
+    }
+  }).catch(err => console.log("promise error happened!", err.message));
+
   function findStocks(stocksArray, data) {
-    const searchArray = JSON.parse(JSON.stringify(stocksArray));
+    // const searchArray = JSON.parse(JSON.stringify(stocksArray));
+    let needsSearch = [];
     const output = {};
     const keys = Object.keys(data);
-    const now = new Date(new Date().setHours(0, 0, 0, 0));
-    now.setDate(now.getDate() - 2);
+    const now = new Date(new Date().setHours(-6, 0, 0, 0));
+    if (moment(new Date()).format('dddd') === 'Saturday') {
+      now.setDate(now.getDate() - 1);
+    }
+    if (moment(new Date()).format('dddd') === 'Sunday') {
+      now.setDate(now.getDate() - 2);
+    }
+    let month = now.getMonth() + 1
+    if (month < 10) {
+      month = '0' + month;
+    }
+    const today = `${now.getFullYear()}-${month}-${now.getDate()}`
     stocksArray.forEach((key, i) => {
-
       if (keys.includes(key)) {
-        console.log("STOCKS MATCH")
-
-        const values = Object.values(data[key].values)
+        const values = Object.values(data[key].values) || []
         values.forEach(day => {
-          const currentDay = new Date(new Date(day.datetime).setHours(0, 0, 0, 0))
-          // console.log(currentDay, now)
-          if (currentDay.getTime() === now.getTime()) {
-            console.log("STOCKS FOUND DAY IN JSON", key)
+          if (today === day.datetime) {
+            needsSearch.push(false);
             output[key] = data[key];
-            if (i === 1) {
-              console.log("STOCKS POP")
-              searchArray.pop();
-            } else {
-              console.log("STOCKS SPLICE")
-
-              searchArray.splice(i, 1);
-            }
           }
 
         })
 
+      } else {
+        needsSearch.push(true);
       }
 
     });
-    return [output, searchArray];
+    return [output, needsSearch];
   }
 }
-
-function isCurrent(data, assets) {
-  const searchArray = JSON.parse(JSON.stringify(assets));
-
-  const now = new Date(new Date().setHours(0, 0, 0, 0));
-  // const stockValues = Object.values(data[asset]);
-  const needsUpdatingStocks = [];
-
-  // console.log(cryptoValues)
-  searchArray.forEach(key => {
-    needsUpdatingStocks.push(key)
-    const values = Object.values(data[key].values)
-    values.forEach(day => {
-      const currentDay = new Date(new Date(day.datetime).setHours(0, 0, 0, 0))
-      if (currentDay.getTime() === now.getTime()) {
-        console.log("FOUND DAY")
-        needsUpdatingStocks.pop()
-      }
-
-    })
-  });
-  return needsUpdatingStocks;
-}
-// cryptoValues.forEach(day => {
-//   const currentDay = new Date(new Date(day[0]).setHours(0, 0, 0, 0));
-//   // console.log(currentDay, now)
-// if (currentDay.getTime() === now.getTime()) {
-//   console.log("FOUND DAY")
-//   current = true;
-// }
-// })
