29f3a44f5c8b6395719fbd8d54b4aab1461d3a62
Ken Cruz <ken@kencruz.ca>
2022-01-23 19:20:46 -0500

    brought back navbar

--- client/src/components/dashboard-layout.js
+++ client/src/components/dashboard-layout.js
@@ -31,7 +31,7 @@ export const DashboardLayout = (props) => {
           {children}
         </Box>
       </DashboardLayoutRoot>
-      {/* GOODBYE FOR NOW :( <DashboardNavbar onSidebarOpen={() => setSidebarOpen(true)} /> */}
+      <DashboardNavbar onSidebarOpen={() => setSidebarOpen(true)} />
       <DashboardSidebar
         onClose={() => setSidebarOpen(false)}
         open={isSidebarOpen}
