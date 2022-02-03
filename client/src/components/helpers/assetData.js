8a3df21ca25d42cb338e9c0abe0e91588e572f4f
Drumshtick <ekodnar@outlook.com>
2022-02-02 18:20:35 -0600

    finished refactor of function for getting crypto data

--- client/src/components/helpers/assetData.js
+++ client/src/components/helpers/assetData.js
@@ -1,10 +1,15 @@
+import axios from 'axios';
+
 const getCryptoData = (dashboardState) => {
 
   const portfolioData = Object.values(dashboardState);
   const allAssets = new Set();
+  const assetData = {};
+  const portfolioCreationDates = [];
   console.log(portfolioData)
 
   portfolioData.forEach(portfolio => {
+    portfolioCreationDates.push(portfolio.portfolioInfo.created_at)
     const assets = Object.values(portfolio.assets);
 
     assets.forEach(asset => {
@@ -14,25 +19,27 @@ const getCryptoData = (dashboardState) => {
     });
   });
   console.log("assets: ", Array.from(allAssets))
-  // cryptoAssets.forEach(asset => {
-  //   axios.post('api/cryptoHistorical', {id: asset.name}).then(res => {
-  //     if (assetData.crypto === undefined) {
-  //       assetData['crypto'] = {};
-  //     }
-  //     const cryptoData = res.data.reverse();
-  //     assetData.crypto[asset.name] = [];
-  //     cryptoData.forEach((day, index) => {
-  //       const currentDay = new Date(new Date(day[0]).setHours(0, 0, 0, 0));
-  //       if (currentDay.getTime() >= oldestDate.getTime()) {
-  //         assetData.crypto[asset.name].push({date: new Date(new Date(day[0]).setHours(0, 0, 0, 0)), data: day[1]});
-  //       }
-  //     });
-  //     setAssetPerformanceCrypto(prev => {
-  //       return {crypto: assetData.crypto}
-  //     })
-  //   }).catch(err => console.log("ERROR in getHistoricalCrypto: ", err));
 
-  // })
+  portfolioCreationDates.sort(function(a, b) {
+    return Date.parse(a) - Date.parse(b);
+  });
+  console.log("portfolio creation dates: ", portfolioCreationDates)
+  const oldestDate = new Date(new Date(portfolioCreationDates[0]).setHours(0, 0, 0, 0));
+  console.log("oldest date: ", oldestDate)
+
+  Array.from(allAssets).forEach(asset => {
+    axios.post('api/cryptoHistorical', {id: asset}).then(res => {
+      const cryptoData = res.data.reverse();
+      assetData[asset] = [];
+      cryptoData.forEach((day, index) => {
+        const currentDay = new Date(new Date(day[0]).setHours(0, 0, 0, 0));
+        if (currentDay.getTime() >= oldestDate.getTime()) {
+          assetData[asset].push({date: new Date(new Date(day[0]).setHours(0, 0, 0, 0)), data: day[1]});
+        }
+      });
+    }).catch(err => console.log("ERROR in getHistoricalCrypto: ", err));
+  })
+  console.log("completed Data: ", assetData)
 };
 
 
