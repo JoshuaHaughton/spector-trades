f2cc8453cce03da6591f93027c479c52291ba17d
Drumshtick <ekodnar@outlook.com>
2022-02-04 18:48:29 -0600

    fixed seeding bu

--- server/db/seeds/03_asset_orders.sql
+++ server/db/seeds/03_asset_orders.sql
@@ -8,10 +8,10 @@ INSERT INTO asset_orders (name, symbol, type, units, price_at_purchase, created_
 VALUES ('Blink Charging Co', 'BLNK', 'Stocks', 2, 35000, '2021-05-20T06:00:00.000Z', 1, 1, false);
 
 INSERT INTO asset_orders (name, symbol, type, units, price_at_purchase, created_at, user_id, portfolio_id, sold) 
-VALUES ('ripple', 'XRP', 'Cryptocurrency', 2, 140, '2021-02-20T06:00:00.000Z', 2, 2, false);
+VALUES ('ripple', 'XRP', 'Cryptocurrency', 2, 140, '2021-02-20T06:00:00.000Z', 1, 2, false);
 
 INSERT INTO asset_orders (name, symbol, type, units, price_at_purchase, created_at, user_id, portfolio_id, sold) 
-VALUES ('ethereum', 'ETH', 'Cryptocurrency', 2, 23453, '2021-03-20T06:00:00.000Z', 2, 2, false);
+VALUES ('ethereum', 'ETH', 'Cryptocurrency', 2, 23453, '2021-03-20T06:00:00.000Z', 1, 2, false);
 
 INSERT INTO asset_orders (name, symbol, type, units, price_at_purchase, created_at, user_id, portfolio_id, sold) 
-VALUES ('ethereum', 'ETH', 'Cryptocurrency', 2, 23453, '2021-04-20T06:00:00.000Z', 2, 2, true);
+VALUES ('ethereum', 'ETH', 'Cryptocurrency', 2, 23453, '2021-04-20T06:00:00.000Z', 1, 2, true);
