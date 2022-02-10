5b6e7a5036155e5654ef31f63945074d4672de6f
Ken Cruz <ken@kencruz.ca>
2022-02-10 14:33:15 -0500

    removed dotenv loading in production

--- server/bin/resetdb.js
+++ server/bin/resetdb.js
@@ -1,5 +1,7 @@
 // load .env data into process.env
-require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require("dotenv").config();
+}
 
 // other dependencies
 const fs = require("fs");
